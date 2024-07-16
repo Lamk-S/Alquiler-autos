4f4ca168ab8f60b0eaf65dbbf67e42f654aea4d7
Lamk-L <kunlancelot@gmail.com>
2024-07-16 18:39:02 -0500

    first commit

---
+++ src/main/java/com/spring/alquiler/app/SpringAlquilerApplication.java
@@ -0,0 +1,13 @@
+package com.spring.alquiler.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAlquilerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAlquilerApplication.class, args);
+	}
+
+}
